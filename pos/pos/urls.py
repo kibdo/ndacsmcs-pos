2283830f6117cff5631277100acb256acef0ab18
kibdo <abuakar21@gmail.com>
2023-01-20 14:19:18 +0000

    pos update

--- pos/pos/urls.py
+++ pos/pos/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls,name="admin-site"),
+    path('admin/', admin.site.urls, name="admin-site"),
     path('', include('posApp.urls')),
 ]
+#urlpatterns += staticfiles_urlpatterns()
