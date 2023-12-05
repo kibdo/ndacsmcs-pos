a3c490ad19346574e880c2c673d9f75391a1c73f
Atiku <abuakar21@gmail.com>
2023-12-05 22:32:59 +0100

    Add .gitignore

--- runserver.py
+++
@@ -1,8 +0,0 @@
-from waitress import serve
-from pos.wsgi import application
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
-
-if __name__ == '__main__':
-    serve(application, host="127.0.0.1", port='8080')
